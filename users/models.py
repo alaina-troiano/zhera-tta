1c3e0bdc5b80e1619dfc5c960280cc157fc52f5b
Alaina Troiano <alaina.troiano@gmail.com>
2014-01-24 20:11:48 -0600

    Added users app with a couple of models; no permissions yet

---
+++ users/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+from archive import Universe
+
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User)
+    universe = models.ForeignKey(Universe)
+    about = models.TextField()
+    contact_number = models.IntegerField(null=True, blank=True)
+    
+    def __unicode__(self):
+        return self.user.first_name # the last_name field is not used
+
+
+class Staff(models.Model):
+    user = models.ForeignKey(User)
+
+
+class Webmaster(models.Model):
+    user = models.ForeignKey(User)
